eac46de9191b0e2472518c043226406fc93776d5
happen-zhang <zhanghaipeng404@gmail.com>
2014-04-01 19:43:41 +0800

    实现service/ post.js/spiderRss 爬取rss内容并存入数据库

---
+++ service/post.js
@@ -0,0 +1,61 @@
+/**
+ * service/post.js
+ */
+
+var rssSiteConfig = require('../config/rss_site_config.json');
+
+var spiderUtil = require('../libs/spider_util');
+var Post = require('../models/post');
+
+/**
+ * 爬取rss内容并存入数据库
+ */
+function spiderRss(callback) {
+  var sites = rssSiteConfig.sites;
+  var rows = 0;
+
+  sites.forEach(function(site) {
+    site.channels.forEach(function(channel) {
+      // 是否爬取
+      if ('false' === channel.isWork) {
+        return ;
+      }
+
+      // 开始爬取
+      spiderUtil.spiderRss(channel.link, function(err, items) {
+        if (err) {
+          return callback(err);
+        }
+
+        // 爬取每条item中的link
+        items.forEach(function(item) {
+          spiderUtil.spiderContent(item.link,
+                                   site.contentTag,
+                                   site.removeTag,
+                                   function(err, content, titlePic) {
+            if (err) {
+              return callback(err);
+            }
+
+            // 保存到数据库中b
+            new Post({
+              link: item.link,
+              title: item.title,
+              titlePic: titlePic,
+              content: content,
+              description: item.description,
+              pubDate: item.pubDate,
+              typeId: channel.typeId
+            }).save(function(err) {
+              callback(err);
+            });
+
+            callback(null, ++rows);
+          });
+        });
+      });
+    });
+  });
+}
+
+exports.spiderRss = spiderRss;
