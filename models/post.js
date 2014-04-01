95620da2ecaa1045e10b10d629fdd55fae5baad2
happen-zhang <zhanghaipeng404@gmail.com>
2014-04-01 19:38:54 +0800

    添加models/ post.js Post模型

---
+++ models/post.js
@@ -0,0 +1,27 @@
+/**
+ * models/post.js
+ */
+
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+  // 链接
+  link: String,
+  // 标题
+  title: { type: String, unique: true},
+  // 图片
+  titlePic: String,
+  // 内容
+  content: String,
+  // 描述
+  description: String,
+  // 发布内容
+  pubDate: {
+    type: Date,
+    default: Date.now
+  },
+  source: String,
+  typeId: Number,
+});
+
+module.exports = mongoose.model('Post', PostSchema);
